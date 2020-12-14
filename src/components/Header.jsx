cc81306480e80a9549c36ffeaa76aecc7b260bda
jesus <Jesusfernandorios93@gmail.com>
2020-12-14 15:58:23 -0700

    created navbar added logo and menu items. working on 3d object image.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './Style/header.css'
+import Navbar from 'react-bootstrap/Navbar'
+
 
 export default function Header() {
     return (
-        <div className="header">
+        <Navbar className="header" bg="dark" variant="dark">
+
             <div className="logo">
                 <h2>Lando Chill</h2>
             </div>
-            <div className="social__buttons">
-                <h3>Button</h3>
-                <h3>Button</h3>
-                <h3>Button</h3>
+            <div className="nav__items">
+                <h3 className="menu__item">Button</h3>
+                <h3 className="menu__item">Button</h3>
+                <h3 className="menu__item">Button</h3>
+                <h3 className="menu__item">Button</h3>
             </div>
-        </div>
+        </Navbar>
     )
 }
