3d2a62f557a7f8e51fc225ab742676a9fef3df28
jesus <Jesusfernandorios93@gmail.com>
2020-12-15 10:39:13 -0700

    Working on 3d model adding to homescreen. Researching react-three-fiber and drei

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React, { Suspense } from 'react'
 import './App.css';
-import Header from './components/Header'
-import ModelBody from './components/ModelBody'
+import Header from './components/Header';
+import { Canvas } from 'react-three-fiber';
+
+import { useGLTF } from 'drei'
+
+
+const Model = () => {
+  const gltf = useGLTF('/dodecahedron.gltf');
+  return <primitive object={gltf.scene} dispose={null}/>
+}
 
 function App() {
+
   return (
-    <div className="App">
+    <>
       <Header/>
-      <ModelBody/>
-    </div>
+      <Canvas
+      colorManagement
+      camera={{position:[0,0,20], fov:70}}
+      >
+        <Suspense fallback={null}>
+          <mesh position={[0,250,0]}>
+            <Model/>
+          </mesh>
+        </Suspense>
+      </Canvas>
+    </>
+    
   );
 }
 
--- src/components/ModelBody.jsx
+++ src/components/ModelBody.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import {Canvas} from 'react-three-fiber'
+import { useGLTF} from 'drei'
+
+const Model = () => {
+    const gltf = useGLTF('/dodecahedron.gtlf', true)
+    return <primitive object={gltf.scene} dispose={null}/>
+}
 
 export default function ModelBody() {
     return (
-        <div>
-            ModelBody
-        </div>
+        <Canvas
+        colorManagement
+        camera={{position: [0,0, 10], fov: 70}}
+        >
+            <Suspense fallback={null}>
+                <mesh position={[0,0,0]}>
+                    <Model/>
+                </mesh> 
+            </Suspense>
+            
+        </Canvas>
     )
 }
