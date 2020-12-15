4c5a9a823440f5172b1b3b23c26675616ba49294
jesus <Jesusfernandorios93@gmail.com>
2020-12-15 11:57:26 -0700

    created dodecahedron model. Now tinkering with rotation and size of shape.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useRef } from 'react'
 import './App.css';
 import Header from './components/Header';
-import { Canvas } from 'react-three-fiber';
-
-import { useGLTF } from 'drei'
+import { Canvas, useFrame } from 'react-three-fiber';
 
 
-const Model = () => {
-  const gltf = useGLTF('/dodecahedron.gltf');
-  return <primitive object={gltf.scene} dispose={null}/>
+const Dodecahedron = ( {...props}) => {
+  return(
+    <mesh {...props}>
+      <Suspense fallback={null}>
+        <dodecahedronBufferGeometry />
+        <meshStandardMaterial roughness={0.75} emissive="gray"/>
+      </Suspense>
+    </mesh>
+  )
+}
+
+const Content = () => {
+ const ref = useRef()
+ useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
+ return(
+    <group ref={ref}>
+      <Dodecahedron position={[0,0,0]}/>
+    </group>
+ )
 }
 
 function App() {
@@ -17,14 +31,12 @@ function App() {
     <>
       <Header/>
       <Canvas
-      colorManagement
-      camera={{position:[0,0,20], fov:70}}
+      camera={{position:[0,0,0,0]}}
       >
-        <Suspense fallback={null}>
-          <mesh position={[0,250,0]}>
-            <Model/>
-          </mesh>
-        </Suspense>
+        <pointLight color='green'/>
+        <pointLight position={[10, 10, -10]} color='orange' />
+        <pointLight position={[10, 10, -10]} color='lightblue' />
+        <Content/>
       </Canvas>
     </>
     
