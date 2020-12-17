a3ade7b71ad5cc9296d89160e0981c8d9064212c
jesus <Jesusfernandorios93@gmail.com>
2020-12-17 14:39:56 -0700

    installed react use guesture and react spring for dragable object. Created state for position and useRef to obtain position data from object

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useState } from 'react'
 import Header from './components/Header';
 import { Canvas, useFrame, useThree } from 'react-three-fiber';
-import { OrbitControls, Html} from '@react-three/drei'
-import { useGesture } from 'react-use-gesture'
-import {useSpring, a} from 'react-spring/three'
-
+import { OrbitControls } from '@react-three/drei'
+import  { useDrag } from 'react-use-gesture'
 import './components/Style/app.css'
 
 
-const Dodecahedron = ({...props}) => {
-  const { size , viewport } = useThree();
-  const aspect = size.width / viewport.width 
-  const [spring, setSpring] = useSpring(() => ({ scale: [1,1,1], position:[0,0,0], rotation: [0,0,0], config: {friction: 10}}))
-  const bind = useGesture({
-    onDrag: ({offset: [x,y]}) => ({position: [x / aspect, -y /aspect, 0], rotation: [y / aspect, x / aspect, 0] }),
-    onHover: ({hovering}) => setSpring({scale: hovering ? [1.2, 1.2, 1.2] : [1,1,1] })
+const Dodecahedron = () => {
+  const ref = useRef();
+  const [position, setPosition] = useState([0,0,0])
+  const { size, viewport } = useThree();
+  const aspect = size.width / viewport.width;
+
+  useFrame(() => {
+    ref.current.rotation.z = ref.current.rotationx += 0.003
   })
+
+  const bind = useDrag(({ offset: [x,y] }) => {
+    const [,,z] = position;
+    setPosition([x / aspect, -y / aspect, z]);
+  }, { pointerEvents: true})
+
   return(
-    <a.mesh {...props} {...spring} {...bind()} castShadow >
+    <mesh position={position} {...bind()} ref={ref}>
       <Suspense fallback={null}>
         <dodecahedronBufferGeometry />
         <meshStandardMaterial roughness={0.75} emissive="gray"/>
-        <Html scaleFactor={10}>
-          <div className="facebook">
-            <h1>Facebook</h1>
-          </div>
-        </Html>
       </Suspense>
-    </a.mesh>
+    </mesh>
   )
-}
-
-const Content = () => {
-
- const ref = useRef()
- useFrame(() => (ref.current.rotation.x = ref.current.rotation.y = ref.current.rotation.z += 0.01))
- return(
-    <group ref={ref}>
-      <Dodecahedron position={[0,0,0]}/>
-    </group>
- )
 }
 
 function App() {
@@ -48,12 +37,12 @@ function App() {
     <>
       <Header/>
       <Canvas
-      camera={{position:[0,0,7], fov:60}}
+      camera={{position:[0,0,7.5]}}
       >
         <pointLight color='white'/>
         <pointLight position={[10, 10, -10]} color='white' />
         <pointLight position={[-10, -10, 10]} color='white' />
-        <Content/>
+        <Dodecahedron/>
       </Canvas>
     </>
     
