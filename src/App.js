1318c1e63a586e22d12afa0ea089752b92a67586
jesus <Jesusfernandorios93@gmail.com>
2020-12-18 13:32:06 -0700

    client only wanted object to rotate :P not drag. fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useRef, useState} from 'react'
 import Header from './components/Header';
 import { Canvas, useFrame, useThree } from 'react-three-fiber';
-import { useSpring, a} from '@react-spring/three'
 import  { useDrag } from 'react-use-gesture'
+import { OrbitControls } from '@react-three/drei'
 import './components/Style/app.css'
 
 
@@ -11,18 +11,19 @@ const Dodecahedron = () => {
   const [position, setPosition] = useState([0, 0, 0]);
   const { size, viewport } = useThree();
   const aspect = size.width / viewport.width;
+
   useFrame(() => {
-    ref.current.rotation.z = ref.current.rotation.x += 0.01
+    ref.current.rotation.z = ref.current.rotation.x += 0.001
   })
 
-  const bind = useDrag(({ offset: [x, y] }) => {
-    const [,, z] = position;
-    setPosition([x / aspect, -y / aspect, z]);
-}, { pointerEvents: true });
+  // const bind = useDrag(({ offset: [x, y] }) => {
+  //   const [,, z] = position;
+  //   setPosition([x / aspect, -y / aspect, z]);
+  // }, { pointerEvents: true });
 
   return(
     
-    <mesh position={position} {...bind()} ref={ref}>
+    <mesh position={position} ref={ref} >
       <dodecahedronBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" emissive="gray"/>
     </mesh>
@@ -36,10 +37,11 @@ function App() {
     <>
       <Header/>
       <Canvas
-      camera={{position:[0,0,7.5]}}
+      camera={{position:[0,0,7.5], fov: 50}}
       >
         <spotLight intensity={1.2} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
         <Dodecahedron/>
+        <OrbitControls/>
       </Canvas>
     </>
     
